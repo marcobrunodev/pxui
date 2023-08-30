5b9eac74ece1e9e1ce8dd987387a3a94c330edac
Marco Bruno <marco.bruno.br@gmail.com>
2023-08-30 22:41:47 +0200

    :rocket: Add the PxUI logo

--- docs/assets/preview-2e82f60c.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-77378617.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-7677c909.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-7677c909.js","./iframe-77378617.js","./index-c4905b50.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-71c6f05f.js","./chunk-6P7RB4HF-0bed2682.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-2e82f60c.js.map
