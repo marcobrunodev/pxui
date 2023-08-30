5c6faf1e87e6ad6e47cd3fe2530c66667d99025e
Marco Bruno <marco.bruno.br@gmail.com>
2023-08-30 22:40:47 +0200

    :pencil2: Change UI w3b3.gg to PxUI

--- .storybook/Theme.ts
+++ .storybook/Theme.ts
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming'
 export default create({
   fontBase: '"Silkscreen", monospace',
   base: 'dark',
-  brandTitle: 'UI w3b3.gg',
+  brandTitle: 'PxUI',
   brandUrl: 'https://marcobrunodev.github.io/pxui',
   brandImage: 'https://marcobrunodev.github.io/pxui/pxui.png',
   brandTarget: '_self'
