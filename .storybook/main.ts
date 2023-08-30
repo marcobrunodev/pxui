7ddedf3650649c7a28d4663bd912add567c3bc31
Marco Bruno <marco.bruno.dev@gmail.com>
2023-08-30 21:54:53 +0200

    :wrench: Add mergeConfig on main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -12,6 +13,20 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: true,
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      optimizeDeps: {
+        include: [
+          '@storybook/addon-a11y/preview.js',
+          '@storybook/addon-actions/preview.js',
+          '@storybook/addon-backgrounds/preview.js',
+          'babel-plugin-open-source/script.js',
+          'chromatic/isChromatic',
+          'storybook-dark-mode',
+        ],
+      },
+    });
   },
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path'
-import { defineConfig, mergeConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
 
@@ -17,19 +17,5 @@ export default defineConfig({
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
     }
-  },
-  async viteFinal(config, { configType }) {
-    return mergeConfig(config, {
-      optimizeDeps: {
-        include: [
-          '@storybook/addon-a11y/preview.js',
-          '@storybook/addon-actions/preview.js',
-          '@storybook/addon-backgrounds/preview.js',
-          'babel-plugin-open-source/script.js',
-          'chromatic/isChromatic',
-          'storybook-dark-mode',
-        ],
-      },
-    });
-  },
+  }
 })
