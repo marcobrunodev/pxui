ae30132aa03ca1e84cd964e14d67671fc2af453c
Marco Bruno <marco.bruno.br@gmail.com>
2023-09-07 10:05:11 +0200

    :rocket: Deploy storybook with the background XClose in black

---
+++ docs/assets/02.XAlert.withoutClose.stories-a610978e.js
@@ -0,0 +1,80 @@
+import{A as M}from"./XAlert-963416f7.js";import"./styled-components.browser.esm-18f716b2.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./XClose-2df4a64d.js";import"./XContainer.styles-f2b40569.js";import"./XText-efcb0c58.js";import"./XText.styles-fae9fb9e.js";const X={title:"02 - components/XAlert/02 - Without Icon Close",component:M},e={args:{isOpen:!0,children:"Your text or component(s) here",errorMessage:!1,iconClose:!1,simple:!1,primary:!1,success:!1,warning:!1,error:!1,disabled:!1}},s={args:{isOpen:!0,children:"Your text or component(s) here",errorMessage:!1,iconClose:!1,simple:!1,primary:!0,success:!1,warning:!1,error:!1,disabled:!1}},r={args:{isOpen:!0,children:"Your text or component(s) here",errorMessage:!1,iconClose:!1,simple:!1,primary:!1,success:!0,warning:!1,error:!1,disabled:!1}},a={args:{isOpen:!0,children:"Your text or component(s) here",errorMessage:!1,iconClose:!1,simple:!1,primary:!1,success:!1,warning:!0,error:!1,disabled:!1}},o={args:{isOpen:!0,children:"Your text or component(s) here",errorMessage:!1,iconClose:!1,simple:!1,primary:!1,success:!1,warning:!1,error:!0,disabled:!1}},n={args:{isOpen:!0,children:"Your text or component(s) here",errorMessage:!1,iconClose:!1,simple:!1,primary:!1,success:!1,warning:!1,error:!1,disabled:!0}};var l,t,i;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    children: 'Your text or component(s) here',
+    errorMessage: false,
+    iconClose: false,
+    simple: false,
+    primary: false,
+    success: false,
+    warning: false,
+    error: false,
+    disabled: false
+  }
+}`,...(i=(t=e.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};var c,f,p;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    children: 'Your text or component(s) here',
+    errorMessage: false,
+    iconClose: false,
+    simple: false,
+    primary: true,
+    success: false,
+    warning: false,
+    error: false,
+    disabled: false
+  }
+}`,...(p=(f=s.parameters)==null?void 0:f.docs)==null?void 0:p.source}}};var u,m,d;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    children: 'Your text or component(s) here',
+    errorMessage: false,
+    iconClose: false,
+    simple: false,
+    primary: false,
+    success: true,
+    warning: false,
+    error: false,
+    disabled: false
+  }
+}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var g,h,C;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    children: 'Your text or component(s) here',
+    errorMessage: false,
+    iconClose: false,
+    simple: false,
+    primary: false,
+    success: false,
+    warning: true,
+    error: false,
+    disabled: false
+  }
+}`,...(C=(h=a.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};var W,b,x;o.parameters={...o.parameters,docs:{...(W=o.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    children: 'Your text or component(s) here',
+    errorMessage: false,
+    iconClose: false,
+    simple: false,
+    primary: false,
+    success: false,
+    warning: false,
+    error: true,
+    disabled: false
+  }
+}`,...(x=(b=o.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var y,O,w;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    children: 'Your text or component(s) here',
+    errorMessage: false,
+    iconClose: false,
+    simple: false,
+    primary: false,
+    success: false,
+    warning: false,
+    error: false,
+    disabled: true
+  }
+}`,...(w=(O=n.parameters)==null?void 0:O.docs)==null?void 0:w.source}}};const j=["WithoutCloseIcon","PrimaryWithoutClose","SuccessWithoutClose","WarningWithoutClose","ErrorWithoutClose","DisableWithoutClosed"];export{n as DisableWithoutClosed,o as ErrorWithoutClose,s as PrimaryWithoutClose,r as SuccessWithoutClose,a as WarningWithoutClose,e as WithoutCloseIcon,j as __namedExportsOrder,X as default};
+//# sourceMappingURL=02.XAlert.withoutClose.stories-a610978e.js.map
---
+++ docs/assets/XClose-2df4a64d.js
@@ -0,0 +1,4 @@
+import{s as o,j as t}from"./styled-components.browser.esm-18f716b2.js";const A="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAAAhUlEQVQ4T8WVSw7AIAgFy/0PXa0JhiCvYALKEsw4/pAeEG8PVJN56mGNM5PfwHQwA4HIIscL0+aLcTp4F6jVtfk0TgcjoDAwLweqz/xxMGtqs/BKvL0tAyPznxc7SuQZl4HLtiIdHD591dzq77E4nNF/o13tXq8wZt4yv9ePkbn37x3/8xqnAcPv7jztbQAAAABJRU5ErkJggg==",a=o.img.attrs({src:A,alt:"Close icon"})`
+  cursor: var(--cursor-pointer);
+`,r={XClose:a},s=({onClick:e})=>t.jsx(r.XClose,{onClick:e});try{s.displayName="XClose",s.__docgenInfo={description:"",displayName:"XClose",props:{onClick:{defaultValue:null,description:"",name:"onClick",required:!0,type:{name:"MouseEventHandler<HTMLImageElement>"}}}}}catch{}export{s as X,a};
+//# sourceMappingURL=XClose-2df4a64d.js.map
