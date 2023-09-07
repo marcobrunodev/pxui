1dff66123fb0a774ba0738aa42dc5800381f23db
Marco Bruno <marco.bruno.br@gmail.com>
2023-09-07 09:23:11 +0200

    :bug: Add the types as option

--- src/01.icons/XEye/XEye.types.ts
+++ src/01.icons/XEye/XEye.types.ts
@@ -1,9 +1,9 @@
 import { MouseEventHandler } from "react"
 
 type XEyeTypes = {
-  onClick: MouseEventHandler,
-  src: string,
-  alt: string
+  onClick?: MouseEventHandler,
+  src?: string,
+  alt?: string
 }
 
 export default XEyeTypes
