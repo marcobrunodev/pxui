16eae0a6169b211b3812148e6d158a8d1d0564f6
Marco Bruno <marco.bruno.br@gmail.com>
2023-09-07 16:01:25 +0200

    :fire: Remove an import that wasn't using it

--- src/03.formComponents/XInput/XInput.tsx
+++ src/03.formComponents/XInput/XInput.tsx
@@ -1,7 +1,6 @@
 import XInputTypes from './XInput.types'
-import S from './XInput.styles'
-import XEye from '../../01.icons/XEye'
 import XToggleEye from './XToggleEye'
+import S from './XInput.styles'
 
 const XInput = (props: XInputTypes) => props.isPassword ? (
   <S.Password show={props.show} type={props.type} isPassword={props.isPassword}>
