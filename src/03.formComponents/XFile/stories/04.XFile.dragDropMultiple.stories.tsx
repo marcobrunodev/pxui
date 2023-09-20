47db510f12f1975fbbb5e9873821839c0fad608c
Marco Bruno <marco.bruno.br@gmail.com>
2023-09-21 00:11:47 +0200

    :memo: Create the story to XFile with multiple

---
+++ src/03.formComponents/XFile/stories/04.XFile.dragDropMultiple.stories.tsx
@@ -0,0 +1,119 @@
+import { Meta, StoryObj } from '@storybook/react'
+import XFile from '..'
+
+const meta: Meta<typeof XFile> = {
+  title: '03 - Form Components/XFile/04 - Drag and drop with multiple',
+  component: XFile
+}
+export default meta
+
+type Story = StoryObj<typeof XFile>
+
+export const DragAndDrop: Story = {
+  args: {
+    children: 'Choose a file...',
+    files: 'No files...',
+    primary: false,
+    success: false,
+    warning: false,
+    error: false,
+    disabled: false,
+    multiple: true,
+    dragAndDrop: {
+      active: true,
+      children: 'Drop here your file ...',
+      overFeedbackContent: `Drop'em right here!`
+    }
+  }
+}
+
+export const PrimaryDragAndDrop: Story = {
+  args: {
+    children: 'Choose a file...',
+    files: 'No files...',
+    primary: true,
+    success: false,
+    warning: false,
+    error: false,
+    disabled: false,
+    multiple: true,
+    dragAndDrop: {
+      active: true,
+      children: 'Drop here your file ...',
+      overFeedbackContent: `Drop'em right here!`
+    }
+  }
+}
+
+export const SuccessDragAndDrop: Story = {
+  args: {
+    children: 'Choose a file...',
+    files: 'No files...',
+    primary: false,
+    success: true,
+    warning: false,
+    error: false,
+    disabled: false,
+    multiple: true,
+    dragAndDrop: {
+      active: true,
+      children: 'Drop here your file ...',
+      overFeedbackContent: `Drop'em right here!`
+    }
+  }
+}
+
+export const WarningDragAndDrop: Story = {
+  args: {
+    children: 'Choose a file...',
+    files: 'No files...',
+    primary: false,
+    success: false,
+    warning: true,
+    error: false,
+    disabled: false,
+    multiple: true,
+    dragAndDrop: {
+      active: true,
+      children: 'Drop here your file ...',
+      overFeedbackContent: `Drop'em right here!`
+    }
+  }
+}
+
+export const ErrorDragAndDrop: Story = {
+  args: {
+    children: 'Choose a file...',
+    files: 'No files...',
+    primary: false,
+    success: false,
+    warning: false,
+    error: true,
+    disabled: false,
+    multiple: true,
+    dragAndDrop: {
+      active: true,
+      children: 'Drop here your file ...',
+      overFeedbackContent: `Drop'em right here!`
+    }
+  }
+}
+
+export const DisabledDragAndDrop: Story = {
+  args: {
+    children: 'Choose a file...',
+    files: 'No files...',
+    primary: false,
+    success: false,
+    warning: false,
+    error: false,
+    disabled: true,
+    multiple: true,
+    dragAndDrop: {
+      active: true,
+      children: 'Drop here your file ...',
+      overFeedbackContent: `Drop'em right here!`
+    }
+  }
+}
+
