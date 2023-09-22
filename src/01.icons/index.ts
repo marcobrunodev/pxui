0539bf7d18ebee307fed0c26a7860b45ae4b0d9f
Marco Bruno <marco.bruno.br@gmail.com>
2023-09-22 22:56:04 +0200

    :sparkles: Export the icons and compoent on index.ts

--- src/01.icons/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './XArrow'
-export * from './XBrazilFlag'
-export * from './XClose'
-export * from './XDiscord'
-export * from './XEye'
-export * from './XEyeClose'
-export * from './XFlags'
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,102 @@
-export * from './01.icons'
+import XGlobalStyles from './XGlobalStyles'
+
+import XArrow from './01.icons/XArrow'
+import XBrazilFlag from './01.icons/XBrazilFlag'
+import XClose from './01.icons/XClose'
+import XDiscord from './01.icons/XDiscord'
+import XEye from './01.icons/XEye'
+import XEyeClose from './01.icons/XEyeClose'
+import XFlags from './01.icons/XFlags'
+import XLoadingFlag from './01.icons/XLoadingFlag'
+import XTelegram from './01.icons/XTelegram'
+import XTheUnitedKingdomFlag from './01.icons/XTheUnitedKingdomFlag'
+import XTwitch from './01.icons/XTwitch'
+import XTwitter from './01.icons/XTwitter'
+import XYouTube from './01.icons/XYouTube'
+
+import XAlert from './02.components/XAlert'
+import XAvatar from './02.components/XAvatar'
+import XBadges from './02.components/XBadges'
+import XBalloon from './02.components/XBalloon'
+import XButton from "./02.components/XButton"
+import XCard from './02.components/XCard'
+import XContainer from './02.components/XContainer'
+import XLine from './02.components/XLine'
+import XText from './02.components/XText'
+
+import XCheckbox from './03.formComponents/XCheckbox'
+import XError from './03.formComponents/XError'
+import XFieldset from './03.formComponents/XFieldset'
+import XFile from './03.formComponents/XFile'
+import XInput from './03.formComponents/XInput'
+import XLabel from './03.formComponents/XLabel'
+import XRadio from './03.formComponents/XRadio'
+import XRadioFactory from './03.formComponents/XRadioFactory/XRadioFactory'
+
+export {
+  XGlobalStyles,
+  XArrow,
+  XBrazilFlag,
+  XClose,
+  XDiscord,
+  XEye,
+  XEyeClose,
+  XFlags,
+  XLoadingFlag,
+  XTelegram,
+  XTheUnitedKingdomFlag,
+  XTwitch,
+  XTwitter,
+  XYouTube,
+  XAlert,
+  XAvatar,
+  XBadges,
+  XBalloon,
+  XButton,
+  XCard,
+  XContainer,
+  XLine,
+  XText,
+  XCheckbox,
+  XError,
+  XFieldset,
+  XFile,
+  XInput,
+  XLabel,
+  XRadio,
+  XRadioFactory
+}
+
+export default {
+  XGlobalStyles,
+  XArrow,
+  XBrazilFlag,
+  XClose,
+  XDiscord,
+  XEye,
+  XEyeClose,
+  XFlags,
+  XLoadingFlag,
+  XTelegram,
+  XTheUnitedKingdomFlag,
+  XTwitch,
+  XTwitter,
+  XYouTube,
+  XAlert,
+  XAvatar,
+  XBadges,
+  XBalloon,
+  XButton,
+  XCard,
+  XContainer,
+  XLine,
+  XText,
+  XCheckbox,
+  XError,
+  XFieldset,
+  XFile,
+  XInput,
+  XLabel,
+  XRadio,
+  XRadioFactory
+}
